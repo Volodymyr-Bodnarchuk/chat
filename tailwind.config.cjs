685b2bdd381bed185a5a7362a9b9e2f9cbe71bd7
Volodymyr Bodnarchuk <volodymyr.bodnarchuk@blockchainvoucher.com>
2022-09-15 15:06:18 +0300

    chore: minor changes, added phosphor package

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#d6d6d631',
+        secondary: '#efefef1a',
+      },
+    },
   },
   plugins: [],
 };
