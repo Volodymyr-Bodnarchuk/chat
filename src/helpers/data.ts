4566625958677b4314142c2277d78d456b8179ef
Volodymyr Bodnarchuk <volodymyr.bodnarchuk@blockchainvoucher.com>
2022-09-15 19:39:42 +0300

    feat: chat right side, temporary fake data, input component, locale for toLocaleString method

---
+++ src/helpers/data.ts
@@ -0,0 +1,102 @@
+const data = {
+  Josefina: {
+    name: 'Josefina',
+    img: 'https://avataaars.io/?avatarStyle=Circle&topType=LongHairBob&accessoriesType=Blank&hairColor=Blonde&facialHairType=Blank&clotheType=ShirtVNeck&clotheColor=Blue02&eyeType=Default&eyebrowType=Angry&mouthType=ScreamOpen&skinColor=Light',
+    messages: [
+      {
+        id: 1,
+        text: 'Hello Volo!',
+        timeStamp: new Date().toLocaleDateString(),
+        forwarded: true,
+      },
+      {
+        id: 2,
+        text: 'Hy Josefina!',
+        timeStamp: new Date().toLocaleDateString(),
+      },
+      {
+        id: 3,
+        text: 'How are you Volo?',
+        timeStamp: new Date().toLocaleDateString(),
+        forwarded: true,
+      },
+      {
+        id: 4,
+        text: 'Thanks, i`m okay',
+        timeStamp: new Date().toLocaleDateString(),
+      },
+      {
+        id: 5,
+        text: 'You?',
+        timeStamp: new Date().toLocaleDateString(),
+      },
+    ],
+  },
+  'Alice Freeman': {
+    name: 'Alice Freeman',
+    img: 'https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraight&accessoriesType=Wayfarers&hairColor=Auburn&facialHairType=Blank&clotheType=BlazerShirt&eyeType=Close&eyebrowType=UpDown&mouthType=ScreamOpen&skinColor=DarkBrown',
+    messages: [
+      {
+        id: 1,
+        text: 'Hello Volo!',
+        timeStamp: new Date().toLocaleDateString(),
+        forwarded: true,
+      },
+      {
+        id: 2,
+        text: 'Hy Alice Freeman!',
+        timeStamp: new Date().toLocaleDateString(),
+      },
+      {
+        id: 3,
+        text: 'How are you Volo?',
+        timeStamp: new Date().toLocaleDateString(),
+        forwarded: true,
+      },
+      {
+        id: 4,
+        text: 'Thanks, i`m okay',
+        timeStamp: new Date().toLocaleDateString(),
+      },
+      {
+        id: 5,
+        text: 'You?',
+        timeStamp: new Date().toLocaleDateString(),
+      },
+    ],
+  },
+  Velazquez: {
+    name: 'Velazquez',
+    img: 'https://avataaars.io/?avatarStyle=Circle&topType=ShortHairDreads01&accessoriesType=Blank&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=Pink&eyeType=Default&eyebrowType=RaisedExcitedNatural&mouthType=Smile&skinColor=Light',
+    messages: [
+      {
+        id: 1,
+        text: 'Hello Volo!',
+        timeStamp: new Date().toLocaleDateString(),
+        forwarded: true,
+      },
+      {
+        id: 2,
+        text: 'Hy Velazquez!',
+        timeStamp: new Date().toLocaleDateString(),
+      },
+      {
+        id: 3,
+        text: 'How are you Volo?',
+        timeStamp: new Date().toLocaleDateString(),
+        forwarded: true,
+      },
+      {
+        id: 4,
+        text: 'Thanks, i`m okay',
+        timeStamp: new Date().toLocaleDateString(),
+      },
+      {
+        id: 5,
+        text: 'You?',
+        timeStamp: new Date().toLocaleDateString(),
+      },
+    ],
+  },
+};
+export { data };
---
+++ src/helpers/locale.ts
@@ -0,0 +1,8 @@
+const locale = {
+  year: '2-digit' as const,
+  month: '2-digit' as const,
+  day: '2-digit' as const,
+  hour: '2-digit' as const,
+  minute: '2-digit' as const,
+};
+export { locale };
